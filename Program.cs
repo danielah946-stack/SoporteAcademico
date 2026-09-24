b0037c3cd8fa0ca12134e40040711fdf8bb1dcc8
danielah946-stack <daniela.hinostroza.946@gmail.com>
2026-09-23 21:50:47 -0500

    Req.5 función con retorno para calcular o asignar prioridad de atención

--- Program.cs
+++ Program.cs
@@ -1,16 +1,114 @@
-﻿static void MostrarMenu()
+﻿using System;
+class Program
 {
-    Console.WriteLine("\n========================================");
-    Console.WriteLine("   SISTEMA DE SOPORTE ACADEMICO");
-    Console.WriteLine("========================================");
-}
-static void Main(string[] args)
-{
-    // Llamamos a la función sin retorno para que dibuje el menú en pantalla
-    MostrarMenu();
 
-    // Aquí abajo continuaría el resto de tu código...
-}
 
+    static bool ValidarCodigo(string codigo)
+    {
+        if (string.IsNullOrWhiteSpace(codigo) || codigo.Length !=9)
+        {
+            return false;
+        }
+
+
+        string codigoMayus= codigo.ToUpper();
+
+        if(codigoMayus.StartsWith("N00"))
+        {
+            return true;
+        }
+        return false;
+    }    
+
+
+    static bool ValidarTipoConsulta(string tipo)
+    {
+        string[] tipoPermitido= { "matricula", "pagos", "plataforma", "constancia"};
+
+
+        if(string.IsNullOrWhiteSpace(tipo))
+        {
+            return false;
+        }
+        string tipoNormalizado = tipo.Trim().ToLower();
+
+
+        switch (tipoNormalizado)
+        {
+           case "matricula":
+           case "pagos":
+           case "constancia":
+           case "plataforma":
+            return true;
+        default:
+            return false;
+           
+        }
+    }    
+   //Requerimiento 5
+   static string CalcularPrioridad(string tipo)
+    {
+        string tipoNormalizado= tipo.Trim(). ToLower();
+        switch(tipoNormalizado)
+        {
+            case "pagos":
+                return"Alta";
+            case "matricula":
+                return "Alta";
+            case "constancia":
+                return "Media";
+            case "plataforma":
+                return "Media";
+            default:
+                return "Sin definir";
+        }
+    }
+
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("--- Modulo de Soporte Academico --- ");
+       
+        //Requerimiento 2
+        string codigo ="";
+       
+        while(true)
+        {
+            Console.WriteLine("Ingresa el codigo de estudiante: ");
+            codigo = Console.ReadLine();
+           
+            if(ValidarCodigo(codigo))
+            {
+                break;
+            }
+            Console.WriteLine("[Error] El codigo debe tener exactamente 9 caracteres y empezar obligatoriamente con 'N00'. ");
+        }
+           
+            Console.WriteLine("Ingresa el nombre del estudiante: ");
+            string nombre = Console.ReadLine();
+           
+            // Requerimiento 3
+            string tipo= "";
+            while (true)
+            {
+                Console.WriteLine("Ingresa el tipo de consulta: ");
+                tipo= Console.ReadLine();
+               
+                if(ValidarTipoConsulta(tipo))
+                {
+                     break;
+                }
+                Console.WriteLine("[Error] El tipo de consulta no es válido. Debe ser uno de la lista permitida.");
+            }
+            // reguirimiento 5
+             string prioridad = CalcularPrioridad(tipo);
+             Console.WriteLine("Prioridad asignada: " + prioridad);
+           
+            Console.WriteLine("Ingresa la descripcion breve: ");
+            string descripcion= Console.ReadLine();
+           
+            Console.WriteLine("\n[exito] Codigo y datos registrados correctamente");
+    }
+    }
 
 
--- obj/Debug/net10.0/SoporteAcademico.AssemblyInfo.cs
+++ obj/Debug/net10.0/SoporteAcademico.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SoporteAcademico")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4a0ae35b8fe90da4bc5b17436e341a09f505fac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed8db838b5de751b66f4bfaa4898ee22d852516e")]
 [assembly: System.Reflection.AssemblyProductAttribute("SoporteAcademico")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SoporteAcademico")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
